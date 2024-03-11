736418b73e136bf73a3c8599bb3bd808b36ca6a6
CrystalDEVtest <161251881+CrystalDEVtest@users.noreply.github.com>
2024-03-11 21:53:56 +0300

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,17 @@
+<?php
+// Используем библиотеку VK для PHP
+require 'vendor/autoload.php';
+use VK\Client\VK as VK;
+
+// Ваш токен от ВКонтакте
+$token = 'ваш_токен';
+
+// Инициализация клиента
+$vk = new VK($token);
+
+// Получаем ссылку на аудио
+$audio_url = $vk->audio->getById('ссылка на аудио');
+
+// Выводим ссылку
+echo $audio_url['url'];
+?>
